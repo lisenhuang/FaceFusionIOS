3e6305b9af965ac6df76871daacaf397abe7741e
Ethan <lisen8018@gmail.com>
2026-08-01 16:24:38 +0800

    Initial Commit

---
+++ FaceFusion/FaceFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFusionApp.swift
+//  FaceFusion
+//
+//  Created by Eason Smith on 01/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct FaceFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
